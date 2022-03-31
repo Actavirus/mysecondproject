c1b3d0edc7c485fe3ffb665e529e39848f38e36a
Arthur <giarto@mail.ru>
2022-03-31 22:12:47 +0300

    eight commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,12 +14,12 @@ func main(){
 	// HTTP-запросы к статическим файлам из папки "./ui/static".
 	// Обратите внимание, что переданный в функцию http.Dir путь
 	// является относительным корневой папке проекта
-	// fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
 	// Используем функцию mux.Handle() для регистрации обработчика для 
 	// всех запросов, которые начинаются с "/static/". Мы убираем
 	// префикс "/static" перед тем как запрос достигнет http.FileServer
-	// mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/snippetbox
 
 go 1.13
+
+require github.com/rogpeppe/godef v1.1.2 // indirect
